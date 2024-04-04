3b3eb03dbfe8362869fcf6ee2c183a307f491f26
amgaina <amgain.abhishek567@gmal.com>
2024-04-04 09:55:51 -0500

    Files added

---
+++ src/HomePage/DivComponents/NewProject/EmptyProject.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function EmptyProject() {
+  return (
+    <div className="new_project m-5">
+
+        <span className="fs-2"> Your Project Fall 2024 </span>
+
+        {/* Container for the empty portion */}
+        <div className="container" style = {{display: 'flex', height: '70vh'}}>
+            <div className='fs-3 text-secondary justify-content-center' style = {{margin: 'auto'}}>
+                WOW Such Empty 😵‍💫 🫙
+            </div>
+        </div>
+        <div className="action_buttons mt-5 d-flex justify-content-end">
+            <button type="button" className="btn btn-success px-4" id = "create_button">
+              Create
+            </button>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default EmptyProject
