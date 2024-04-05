603eecb12025eb46cf4cc87b5df308c8c0c2bb18
Georgey764 <khawasgs@warhawks.ulm.edu>
2024-04-05 12:11:34 -0500

    Changed Project Structure

---
+++ Frontend/ReactApp/src/HomePage/DivComponents/NewProject/EmptyProject.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function EmptyProject({clickCreate}) {
+  
+  return (
+    <div className="new_project m-5">
+
+        <span className="fs-2"> Your Project Fall 2024 </span>
+
+        {/* Container for the empty portion */}
+        <div className="container" style = {{display: 'flex', height: '70vh'}}>
+            <div className='fs-3 text-secondary justify-content-center' style = {{margin: 'auto'}}>
+                WOW Such Empty 😵‍💫 🫙
+            </div>
+        </div>
+        <div className="action_buttons mt-5 d-flex justify-content-end">
+            <button type="button" className="btn btn-success px-4" id = "create_button" onClick = {()=>clickCreate(true) }>
+              Create
+            </button>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default EmptyProject
